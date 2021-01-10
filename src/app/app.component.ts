449b6a2863160a9934c3f71454c8ccc06f218df2
Andrés Garcia <andresgarciatehortua@gmail.com>
2021-01-10 00:18:58 -0500

    Se refactoriza y reduce codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Links de Estudio';
+  selection = '';
+  referencia = '';
+
+  public linkReferencia(seleccion: string = '', link: string = ''): void {
+    this.selection = seleccion;
+    this.referencia = link;
+  }
 }
+
