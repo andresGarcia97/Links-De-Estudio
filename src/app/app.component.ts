aa399d7d70e3f57f057cc808b43684138a03aa2e
Andrés Garcia <andresgarciatehortua@gmail.com>
2021-01-18 21:28:06 -0500

    Se añade barra de contenido, se quita humo del cohete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const scroll$ = fromEvent(document, 'scroll');
+
+const calcularPorcentaje = (event: any) => {
+  const {
+    scrollTop,
+    scrollHeight,
+    clientHeight
+  } = event.target.documentElement;
+  return scrollTop / (scrollHeight - clientHeight) * 100;
+};
+
+const porcentaje = scroll$.pipe(
+  map(event => calcularPorcentaje(event))
+);
 
 @Component({
   selector: 'app-root',
@@ -8,8 +25,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'Links de Estudio';
+  fechaActualizacion = '18/01/2021';
   selection = '';
   referencia = '';
+  porciento: any = porcentaje.subscribe((valor) => {
+    this.porciento = valor;
+  });
 
   public linkReferencia(seleccion: string = '', link: string = '', elemento: any): void {
     this.selection = seleccion;
@@ -22,5 +43,5 @@ export class AppComponent {
       $element.scrollIntoView({ behavior: 'smooth' });
     }
   }
-}
 
+}
