b0cb58f9ca72f675e966d357bb6d2c536199f542
Andrés Garcia <andresgarciatehortua@gmail.com>
2021-02-06 23:08:01 -0500

    se cambian las declaraciones de los componentes por componentes dinamicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ArquitecturasComponent } from './components/arquitecturas/arquitecturas.component';
+import { BuenasPracticasComponent } from './components/buenas-practicas/buenas-practicas.component';
+import { FrameworksComponent } from './components/frameworks/frameworks.component';
+import { MalasPracticasComponent } from './components/malas-practicas/malas-practicas.component';
+import { MetodologiasComponent } from './components/metodologias/metodologias.component';
+import { OtrosComponent } from './components/otros/otros.component';
+import { ParadigmasComponent } from './components/paradigmas/paradigmas.component';
+import { PatronesComponent } from './components/patrones/patrones.component';
+import { RefactorizacionComponent } from './components/refactorizacion/refactorizacion.component';
+import { SectionsComponent } from './components/sections/sections.component';
+import { SqlNosqlComponent } from './components/sql-nosql/sql-nosql.component';
 
 const scroll$ = fromEvent(document, 'scroll');
 
@@ -24,17 +35,66 @@ const porcentaje = scroll$.pipe(
 })
 export class AppComponent {
 
-  fechaActualizacion = '28/01/2021';
-  selection = '';
-  referencia = '';
+  @ViewChild('componentes', { read: ViewContainerRef }) componentes!: ViewContainerRef;
+
+  fechaActualizacion = '06/02/2021';
+
   porciento: any = porcentaje.subscribe((valor) => {
     this.porciento = valor;
   });
 
-  public linkReferencia(seleccion: string = '', link: string = ''): void {
-    this.selection = seleccion;
-    this.referencia = link;
-    this.scrollToElement(null);
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  private crearComponent(component: any): void {
+    const componentes = this.componentFactoryResolver.resolveComponentFactory(
+      component
+    );
+    this.componentes.clear();
+    this.componentes.createComponent(componentes);
+  }
+
+  public createComponentArquitecturas(): void {
+    this.crearComponent(ArquitecturasComponent);
+  }
+
+  public createComponentBuenasPracticas(): void {
+    this.crearComponent(BuenasPracticasComponent);
+  }
+
+  public createComponentFrameworks(): void {
+    this.crearComponent(FrameworksComponent);
+  }
+
+  public createComponentMalasPracticas(): void {
+    this.crearComponent(MalasPracticasComponent);
+  }
+
+  public createComponentMetodologias(): void {
+    this.crearComponent(MetodologiasComponent);
+  }
+
+  public createComponentOtros(): void {
+    this.crearComponent(OtrosComponent);
+  }
+
+  public createComponentParadigmas(): void {
+    this.crearComponent(ParadigmasComponent);
+  }
+
+  public createComponentPatrones(): void {
+    this.crearComponent(PatronesComponent);
+  }
+
+  public createComponentRefactorizacion(): void {
+    this.crearComponent(RefactorizacionComponent);
+  }
+
+  public createComponentSections(): void {
+    this.crearComponent(SectionsComponent);
+  }
+
+  public createComponentSqlNoSql(): void {
+    this.crearComponent(SqlNosqlComponent);
   }
 
   public scrollToElement($element: any): void {
