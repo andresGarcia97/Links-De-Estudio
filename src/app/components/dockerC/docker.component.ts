34584a2023bddf3ce584a865f51f0b2c4794c5b0
Andrés Garcia <andresgarciatehortua@gmail.com>
2022-01-09 14:38:54 -0500

    refactor: se mueve el contenido de 5 componentes

--- src/app/components/dockerC/docker.component.ts
+++ src/app/components/dockerC/docker.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DOCKER } from 'src/app/models/content';
 import { LinkReferencia } from 'src/app/models/linkReferencia';
 
 @Component({
@@ -7,8 +8,6 @@ import { LinkReferencia } from 'src/app/models/linkReferencia';
 })
 export class DockerComponent extends LinkReferencia {
 
-  constructor() {
-    super();
-  }
+  items = DOCKER;
 
 }
