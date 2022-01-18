a3663e5ea0f906b0dd4b452919bf01cf2a7af71c
Andrés Garcia <andresgarciatehortua@gmail.com>
2022-01-17 23:02:59 -0500

    refactor: se mueve el contenido de 2 componentes

--- src/app/components/nube/nube.component.ts
+++ src/app/components/nube/nube.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NUBE } from 'src/app/models/content';
 import { LinkReferencia } from 'src/app/models/linkReferencia';
 
 @Component({
@@ -7,8 +8,6 @@ import { LinkReferencia } from 'src/app/models/linkReferencia';
 })
 export class NubeComponent extends LinkReferencia {
 
-  constructor() {
-    super();
-  }
+  items = NUBE;
 
 }
