09835213c90aff2183608c2154084e72be27a02d
Andrés Garcia <andresgarciatehortua@gmail.com>
2023-02-22 23:19:23 -0500

    fix | feat: Se agrega keylistener para intercambiar items con las flechas, Se corrige presentacion de algunos temas

--- src/app/components/core/section/section.component.ts
+++ src/app/components/core/section/section.component.ts
@@ -1,11 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Item, Referencia } from 'src/app/models/models';
+
+const keypressed$ = fromEvent(document, "keydown")
+
+const porcentaje = keypressed$.pipe(
+  map(event => event)
+);
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html'
 })
 export class SectionComponent implements OnInit {
+
+  listenArrows: any = porcentaje.subscribe((valor) => {
+    const event = valor as KeyboardEvent;
+    const key = event.key;
+
+    if("ArrowRight" === key){
+      this.getNextElement();
+    }
+
+    else if("ArrowLeft" === key){
+      this.getBeforeElement();
+    }   
+  });
 
   @Input() components: Map<string, Referencia> = new Map<string, Referencia>([]);
   @Input() tittles = new Map<string, string>([]);
@@ -29,7 +50,7 @@ export class SectionComponent implements OnInit {
   public getNextElement(): void {
     const key = this.selection;
 
-    if (key !== '' && this.itemSelected <= this.lengthItems) {
+    if (key !== '' && this.itemSelected < this.lengthItems) {
       this.itemSelected = this.getIndexItemSelected(key) + 1;
       this.getItemByIndex(this.itemSelected);
       return;
