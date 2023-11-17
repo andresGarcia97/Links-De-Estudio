01b2c0c4e1710c70015e9f0779ccb44bfec84f6f
Andres Garcia <andresgarciatehortua@gmail.com>
2023-11-16 23:54:14 -0500

    feat: Se agregan temas relacionados en la seccion de arquitecturas - Mejoras en la redacción de algunos temas

--- src/app/components/arquitecturas/arquitecturas.component.ts
+++ src/app/components/arquitecturas/arquitecturas.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ARQUITECTURAS } from 'src/app/models/content/content1';
-import { ARQUITECTURAS_REF } from 'src/app/models/linkReferencia';
+import { ANALISIS_KEY, ARQUITECTURAS_REF, LinkReferencia, METODOLOGIAS_KEY, PATRONES_KEY, UML_KEY } from 'src/app/models/linkReferencia';
 
 @Component({
   selector: 'app-arquitecturas',
@@ -13,6 +13,8 @@ export class ArquitecturasComponent implements OnInit {
   components = new Map([ ...ARQUITECTURAS_REF ]);
 
   itemStart = '';
+
+  relatedSections = new Map<string, string>([]);
 
   tittles = new Map<string, string>([
     ['definicion', 'Definición'],
@@ -36,6 +38,11 @@ export class ArquitecturasComponent implements OnInit {
 
   ngOnInit(): void {
     this.itemStart = history?.state?.newItem;
+    const routes = new LinkReferencia().routesAndSections;
+    this.relatedSections.set(METODOLOGIAS_KEY, routes.get(METODOLOGIAS_KEY)!);
+    this.relatedSections.set(PATRONES_KEY    , routes.get(PATRONES_KEY)!);
+    this.relatedSections.set(ANALISIS_KEY    , routes.get(ANALISIS_KEY)!);
+    this.relatedSections.set(UML_KEY         , routes.get(UML_KEY)!);
   }
 
 }
