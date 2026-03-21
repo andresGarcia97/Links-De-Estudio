1de7431071a9556720a633ea21b4dd522a1ffc95
Andres Garcia <andresgarciatehortua@gmail.com>
2026-03-21 15:40:02 -0500

    componente poo migrado

--- src/app/components/poo/poo.component.ts
+++ src/app/components/poo/poo.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { POO } from 'src/app/models/content/content4';
+import { ActivatedRoute } from '@angular/router';
 import {
   ARQUITECTURAS_KEY, ESTRATEGIAS_DESARROLLO_KEY, LinkReferencia, PARADIGMAS_KEY, PATRONES_KEY, POO_REF, PRINCIPIOS_KEY, UML_KEY }
 from 'src/app/models/linkReferencia';
-import { PreviousAndNextSection } from 'src/app/models/models';
+import { Item, PreviousAndNextSection } from 'src/app/models/models';
+import { POO_PATH } from 'src/app/models/relationsSummary';
 
 @Component({
     selector: 'app-poo',
@@ -12,7 +13,7 @@ import { PreviousAndNextSection } from 'src/app/models/models';
 })
 export class PooComponent implements OnInit {
 
-  items = POO;
+  items: Item[] = [];
 
   components = POO_REF;
 
@@ -24,7 +25,10 @@ export class PooComponent implements OnInit {
 
   previousAndNextSection!: PreviousAndNextSection;
 
+  constructor(private route: ActivatedRoute) {}
+
   ngOnInit(): void {
+    this.items = this.route.snapshot.data[POO_PATH.propertyNameData];
     this.itemStart = history?.state?.newItem;
     const routes = new LinkReferencia().routesAndSections;
     this.relatedSections.set(ESTRATEGIAS_DESARROLLO_KEY, routes.get(ESTRATEGIAS_DESARROLLO_KEY)!);
--- src/app/components/poo/poo.module.ts
+++ src/app/components/poo/poo.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { PooComponent } from './poo.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CoreModule } from '../core/core.module';
+import { POO_PATH } from 'src/app/models/relationsSummary';
+import { contentResolver } from '../core/content.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    component: PooComponent
+    component: PooComponent,
+    resolve: { [POO_PATH.propertyNameData]: contentResolver(POO_PATH.file) }
   }
 ];
 
