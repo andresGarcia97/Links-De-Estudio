c90790fd7e4169549828404d623cfa58341f3c58
Andres Garcia <andresgarciatehortua@gmail.com>
2026-03-21 20:43:49 -0500

    componente observabilidad migrado

--- src/app/components/observabilidad/observabilidad.component.ts
+++ src/app/components/observabilidad/observabilidad.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { OBSERVABILIDAD } from 'src/app/models/content/content8';
+import { ActivatedRoute } from '@angular/router';
 import { OBSERVABILIDAD_REF, LinkReferencia, NUBE_KEY, CALIDAD_KEY, CI_CD_KEY, APIS_KEY, HARDWARE_KEY }
     from 'src/app/models/linkReferencia';
-import { PreviousAndNextSection } from 'src/app/models/models';
+import { Item, PreviousAndNextSection } from 'src/app/models/models';
+import { OBSERVABILIDAD_PATH } from 'src/app/models/relationsSummary';
 
 @Component({
     selector: 'app-observabilidad',
@@ -11,7 +12,7 @@ import { PreviousAndNextSection } from 'src/app/models/models';
 })
 export class ObservabilidadComponent implements OnInit {
 
-    items = OBSERVABILIDAD;
+    items: Item[] = [];
 
     components = OBSERVABILIDAD_REF;
 
@@ -23,17 +24,20 @@ export class ObservabilidadComponent implements OnInit {
 
     previousAndNextSection!: PreviousAndNextSection;
 
-    ngOnInit(): void {
-        this.itemStart = history?.state?.newItem;
-        const routes = new LinkReferencia().routesAndSections;
-        this.relatedSections.set(HARDWARE_KEY, routes.get(HARDWARE_KEY)!);
-        this.relatedSections.set(CALIDAD_KEY,  routes.get(CALIDAD_KEY)!);
-        this.relatedSections.set(CI_CD_KEY,    routes.get(CI_CD_KEY)!);
-        this.relatedSections.set(NUBE_KEY,     routes.get(NUBE_KEY)!);
-        this.previousAndNextSection = new PreviousAndNextSection(
-            NUBE_KEY, routes.get(NUBE_KEY)!,
-            APIS_KEY, routes.get(APIS_KEY)!
-        );
-    }
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.items = this.route.snapshot.data[OBSERVABILIDAD_PATH.propertyNameData];
+    this.itemStart = history?.state?.newItem;
+    const routes = new LinkReferencia().routesAndSections;
+    this.relatedSections.set(HARDWARE_KEY, routes.get(HARDWARE_KEY)!);
+    this.relatedSections.set(CALIDAD_KEY,  routes.get(CALIDAD_KEY)!);
+    this.relatedSections.set(CI_CD_KEY,    routes.get(CI_CD_KEY)!);
+    this.relatedSections.set(NUBE_KEY,     routes.get(NUBE_KEY)!);
+    this.previousAndNextSection = new PreviousAndNextSection(
+        NUBE_KEY, routes.get(NUBE_KEY)!,
+        APIS_KEY, routes.get(APIS_KEY)!
+    );
+  }
 
 }
--- src/app/components/observabilidad/observabilidad.module.ts
+++ src/app/components/observabilidad/observabilidad.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { ObservabilidadComponent } from './observabilidad.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CoreModule } from '../core/core.module';
+import { OBSERVABILIDAD_PATH } from 'src/app/models/relationsSummary';
+import { contentResolver } from '../core/content.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    component: ObservabilidadComponent
+    component: ObservabilidadComponent,
+    resolve: { [OBSERVABILIDAD_PATH.propertyNameData]: contentResolver(OBSERVABILIDAD_PATH.file) }
   }
 ];
 
