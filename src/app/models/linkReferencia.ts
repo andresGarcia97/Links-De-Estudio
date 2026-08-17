4ded77dc338f724f791ff2db2fac45d317596663
Andres Garcia <andresgarciatehortua@gmail.com>
2026-08-16 23:28:14 -0500

    feat: enhance nube.json with additional references and improved descriptions

--- src/app/models/linkReferencia.ts
+++ src/app/models/linkReferencia.ts
@@ -949,7 +949,9 @@ export const NUBE_REF = new Map<string, Referencia>([
     [Keys.NUBE_NUBE, new Referencia('https://azure.microsoft.com/es-es/overview/what-are-private-public-hybrid-clouds/', 'Tipos de nube', NUBE_KEY, 'Tipos de nube')],
     [Keys.NUBE_IAAS_PAAS_SAAS, new Referencia('https://www.ambit-bst.com/blog/definici%C3%B3n-de-iaas-paas-y-saas-en-qu%C3%A9-se-diferencian', 'IaaS, PaaS, SaaS', NUBE_KEY, 'IaaS, PaaS, SaaS', '2023/12/30', '2025/04/02')],
     [Keys.NUBE_CAAS_DAAS_FAAS, new Referencia('https://aws.plainenglish.io/conquering-the-cloud-unveiling-saas-paas-caas-daas-iaas-and-faas-3789b0ad7018', 'CaaS, DaaS, FaaS', NUBE_KEY, 'CaaS, DaaS, FaaS', '2024/03/18')],
-    [Keys.NUBE_12_FACTORES, new Referencia('https://12factor.net/es/', '12 factores para una aplicación SaaS', NUBE_KEY, '12 - factores')],
+    [Keys.NUBE_12_FACTORES, new Referencia('https://12factor.net/es/', '12 factores para una aplicación SaaS', NUBE_KEY, '12 - factores', '', '2026/08/13')
+        .addRelatedItems(Keys.CD_CI_CI_CD, Keys.SECURITY_ENVINROMENT_VARIABLES, Keys.CD_CI_HIDDEN_LAYERS, Keys.CD_CI_SIGTERM, Keys.TESTING_ADVANCED_ENVIRONMENT)
+    ],
     [Keys.NUBE_MIGRATIONS, new Referencia('https://www.paradigmadigital.com/techbiz/mitos-lift-shift-mentiras-migraciones-cloud/', 'Migraciones hacia la nube y las 6R', NUBE_KEY, 'Migraciones')],
     [Keys.NUBE_CAP_TEOREMA, new Referencia('https://www.ionos.es/digitalguide/servidores/know-how/que-es-el-cap-theorem/', 'Coherencia, Disponibilidad y Tolerancia', NUBE_KEY, 'Teorema CAP')
         .addRelatedItems(Keys.PERSISTENCY_DBMS)
